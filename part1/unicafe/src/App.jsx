5738c68e7941aea2b81cb2da7e764c1e87d4d0bd
projectone <shreeshanjana768@gmail.com>
2025-08-01 21:03:29 +0545

    Part 1d completed all

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,7 @@ const App =() => {
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write the code that humans can understand.',
-    'Premature otimization is the root of all evil.',
+    'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the coe in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
